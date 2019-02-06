d913cf26b68679a5a8ea26ddfe7c4a3165d54a9c
ygor_gmetrix <ygor@gmetrix.net>
2019-02-06 16:55:14 -0700

    Open repository

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: movies/random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name ="Sherek" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1" },
+                new Customer {Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+                        
+            return View(viewModel); //view() will render the html under folder Movie/Random.cshtml
+                                // If pass the movie model as a argument of View() it render the object "movie" inside Random
+
+
+
+
+        }
+
+        // GET: movies/edit
+        public ActionResult Edit(int id)
+        {
+            return Content("Edit="+ id);
+        }
+
+        // GET: movies/index
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex = {0} and SortBy={1}", pageIndex,sortBy));
+        }
+
+        //GET:Movies/released/2014/01
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2})}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
