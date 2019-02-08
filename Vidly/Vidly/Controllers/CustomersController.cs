f31dc6aaf420628da728ff4c23954db0f398fce4
ygor_gmetrix <ygor@gmetrix.net>
2019-02-07 17:14:04 -0700

    Exercise 1

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -3,18 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
 
 namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
         // GET: Customers
-        public ActionResult Index()
+        public ViewResult Index()
         {
+            var customers = GetCustomers();
 
+            return View(customers); //this is the Model
+        }
 
+        // GET:
+        public ViewResult Detail(int id)
+        {
+            var customer = GetCustomers().Where(c => c.Id == id);
 
-            return View();
+            //var customer = customers[id];
+
+            return View(customer); //this is the Model
+        }
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Ygor Andreff"},
+                new Customer { Id = 2, Name = "Daniel Arrieta" },
+                new Customer { Id = 3, Name = "Danny Sejas" }
+            };
         }
     }
 }
